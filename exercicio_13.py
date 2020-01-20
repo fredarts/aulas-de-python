c0174f0558ba20d81c03c4c0ba8d1cf34fe377d1
Frederico Monteiro <fredericomonteiromendesmaia@gmail.com>
2020-01-20 19:31:52 -0300

    adição de mascara no valor do salario

--- exercicio_13.py
+++ exercicio_13.py
@@ -1,4 +1,4 @@
 salario = float(input('qual o salario do funcionario? R$'))
 aumento = salario + (salario * 15 / 100)
 
-print('o salario do funcionario é {}'.format(aumento))
+print('o salario do funcionario é {:.0f}'.format(aumento))
