f4751dc8990dcbf726d385a131e765f4eb6775dc
Frederico Monteiro <fredericomonteiromendesmaia@gmail.com>
2020-01-20 19:29:19 -0300

    correção no nome hipotenusa e adição da mascara

--- aula17.py
+++ aula17.py
@@ -1,5 +1,5 @@
 import math
 co = float(input('digite o cateto oposto'))
 ca = float(input('digite o cateto adjacente'))
-hipotensa = math.hypot(co, ca)
-print('a hipotenusa vai medir {}'.format(hipotensa))
+hipotenusa = math.hypot(co, ca)
+print('a hipotenusa vai medir {:.2f}'.format(hipotenusa))
